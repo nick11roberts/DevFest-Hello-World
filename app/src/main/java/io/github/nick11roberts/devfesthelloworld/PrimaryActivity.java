6810cdaf8d5041b62a1dd1fb57dc2b912d73b895
nick11roberts <nick.roberts.127.0.0.1@gmail.com>
2014-10-10 17:13:50 -0700

    Added some Java for the button.

--- app/src/main/java/io/github/nick11roberts/devfesthelloworld/PrimaryActivity.java
+++ app/src/main/java/io/github/nick11roberts/devfesthelloworld/PrimaryActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class PrimaryActivity extends Activity {
@@ -12,6 +14,17 @@ public class PrimaryActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_primary);
+
+        // Initialize the button.
+        final Button theButton = (Button) findViewById(R.id.the_button);
+
+        // Listen to the button, it has emotions as well.
+        theButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Perform Dropbox-y-type things.
+                
+            }
+        });
     }
 
 
